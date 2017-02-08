be768d8d89372d830ff2918dd380e3cfd15e4c34
Pattie Reaves <pattie@alleyinteractive.com>
2017-02-07 20:05:22 -0500

    Adds comment, link to Huron config.

--- huron.config.js
+++ huron.config.js
@@ -1,5 +1,15 @@
+/**
+ * The Huron configuration file.
+ */
+
+// Requires
 path = require('path');
 
+/**
+ * A configuration object for your Huron settings.
+ * For a detailed explanation,
+ * @see  https://github.com/alleyinteractive/huron/blob/master/config/README.md
+ */
 module.exports = {
   css: [],
   entry: 'index',
