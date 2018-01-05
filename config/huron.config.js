ec7a4ecd322d5298ed7db708ebaa1bd4d914eab1
Owen Stowe <ostowe@gmail.com>
2018-01-05 11:09:10 -0500

    Move around directories to make things more organized
    - Add sample vendor css, js, and image

--- config/huron.config.js
+++ config/huron.config.js
@@ -3,7 +3,8 @@
  */
 
 // Requires
-path = require('path');
+const path = require('path');
+const fs = require('fs');
 
 /**
  * A configuration object for your Huron settings.
@@ -11,10 +12,12 @@ path = require('path');
  * @see  https://github.com/alleyinteractive/huron/blob/master/config/README.md
  */
 module.exports = {
-  css: [],
-  entry: 'index',
-  js: [],
-  kss: 'scss',
+  entry: 'main',
+  css: ['vendor/css/vendor-styles.css'],
+  js: ['vendor/js/vendor-script.js'],
+  assets: fs.readdirSync(path.join(__dirname, '../src/images'))
+    .map((file) => path.join('src/images', file)),
+  kss: 'src/scss',
   kssExtension: '.scss',
   kssOptions: {
     multiline: true,
@@ -24,8 +27,8 @@ module.exports = {
   output: 'partials',
   port: 8080,
   prototypes: ['sample'],
-  root: 'static/prototype',
-  classNames: path.join(__dirname, 'static/css/classNames'),
+  root: 'dist/prototype',
+  classNames: path.join(__dirname, '../dist/css/classNames'),
   templates: {
     rule: {
       test: /\.(hbs|handlebars)$/,
